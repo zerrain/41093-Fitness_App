7eda5bbc06b8e6d565b08f46947f95be90e6b964
zerrain <37678931+zerrain@users.noreply.github.com>
2018-03-24 04:21:54 +1100

    Add files via upload

--- app/src/main/java/grp2/fitness/HomeActivity.java
+++ app/src/main/java/grp2/fitness/HomeActivity.java
@@ -1,17 +1,48 @@
 package grp2.fitness;
 
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.NavigationView;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
-
-/**
- * Created by Zaid on 23/03/2018.
- */
+import android.view.MenuItem;
 
 public class HomeActivity extends AppCompatActivity {
 
+    private DrawerLayout mDrawerLayout;
+    private ActionBarDrawerToggle drawerToggle;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-    super.onCreate(savedInstanceState);
-    setContentView(R.layout.home);
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.home);
+        mDrawerLayout = (DrawerLayout) findViewById(R.id.drawerLayout);
+        drawerToggle = new ActionBarDrawerToggle(this,mDrawerLayout,R.string.open,R.string.close);
+        mDrawerLayout.addDrawerListener(drawerToggle);
+        drawerToggle.syncState();
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        NavigationView navigationView = findViewById(R.id.navMenu);
+        navigationView.setNavigationItemSelectedListener(
+                new NavigationView.OnNavigationItemSelectedListener() {
+                    @Override
+                    public boolean onNavigationItemSelected(MenuItem item) {
+                        item.setChecked(true);
+                        mDrawerLayout.closeDrawers();
+                        //WHAT TO DO ONCE MENU ITEM HAS BEEN SELECTED
+
+                        return true;
+                    }
+                }
+        );
     }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (drawerToggle.onOptionsItemSelected(item)) {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
 }
