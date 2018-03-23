b4501ac5c9b4f23ad5940353e1615aa35daa3a03
zerrain <37678931+zerrain@users.noreply.github.com>
2018-03-23 16:39:52 +1100

    Add files via upload

---
+++ app/src/main/java/grp2/fitness/HomeActivity.java
@@ -0,0 +1,17 @@
+package grp2.fitness;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by Zaid on 23/03/2018.
+ */
+
+public class HomeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.home);
+    }
+}
---
+++ app/src/main/java/grp2/fitness/LoginActivity.java
@@ -0,0 +1,20 @@
+package grp2.fitness;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.login);
+    }
+
+    public void loginHome(View view) {
+        startActivity(new Intent(LoginActivity.this, HomeActivity.class));
+    }
+
+}
