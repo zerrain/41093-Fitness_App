607996eb633306f93554780d3b8f128250276fd8
zerrain <37678931+zerrain@users.noreply.github.com>
2018-05-09 21:29:27 +1000

    -removed tools and share fragments from nav drawer -added calculator, pedometer and heart rate fragments to nav drawer -added logo and name to login screen, fixed some dimensions and alignment, slightly adjusted color

--- app/src/main/java/grp2/fitness/Fragments/HeartRateFragment.java
+++ app/src/main/java/grp2/fitness/Fragments/HeartRateFragment.java
@@ -8,10 +8,10 @@ import android.view.ViewGroup;
 
 import grp2.fitness.R;
 
-public class ToolFragment extends Fragment {
+public class HeartRateFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_tool, container, false);
+        return inflater.inflate(R.layout.fragment_heartrate, container, false);
     }
 }
