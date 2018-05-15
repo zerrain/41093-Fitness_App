75dd7f29d2bf9a1114a703e46939e9c3e53c3445
Campbell <Campbell.M.Gorman@student.uts.edu.au>
2018-05-16 08:41:52 +1000

    Added preferences

--- app/src/main/java/grp2/fitness/Fragments/SettingsFragment.java
+++ app/src/main/java/grp2/fitness/Fragments/SettingsFragment.java
@@ -1,17 +1,13 @@
 package grp2.fitness.Fragments;
 
 import android.os.Bundle;
-import android.support.v4.app.Fragment;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
+import android.support.v7.preference.PreferenceFragmentCompat;
 
 import grp2.fitness.R;
 
-public class SettingsFragment extends Fragment {
+public class SettingsFragment extends PreferenceFragmentCompat {
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_settings, container, false);
+    public void onCreatePreferences(Bundle savedInstanceState, String rootKey) {
+        setPreferencesFromResource(R.xml.preferences, rootKey);
     }
 }
