51d2aad2a9cf27c1fe3c707b80df8a1e547b69bf
zerrain <37678931+zerrain@users.noreply.github.com>
2018-05-26 21:30:54 +1000

    update with calc ui

--- app/src/main/java/grp2/fitness/fragments/CalculatorFragment.java
+++ app/src/main/java/grp2/fitness/fragments/CalculatorFragment.java
@@ -2,16 +2,33 @@ package grp2.fitness.fragments;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v4.view.ViewPager;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Spinner;
+import android.widget.TextView;
 
 import grp2.fitness.NavigationActivity;
 import grp2.fitness.R;
+import grp2.fitness.helpers.UnitConverter;
 
-public class CalculatorFragment extends Fragment {
+public class CalculatorFragment extends Fragment implements
+        TextWatcher{
 
     private int TDEE;
+    private TextView bmiValue;
+    private EditText bmiWeight;
+    private EditText bmiHeight;
+    private EditText energyKJ;
+    private EditText energyCal;
+    private Spinner activityLevel;
     private NavigationActivity activity;
 
     @Override
@@ -25,6 +42,54 @@ public class CalculatorFragment extends Fragment {
         View view = inflater.inflate(R.layout.fragment_calculator, container, false);
         activity = (NavigationActivity) getActivity();
         activity.hideLoadingIcon();
+
+        bmiValue = view.findViewById(R.id.bmiValue);
+        bmiWeight = view.findViewById(R.id.bmiWeight);
+        bmiHeight = view.findViewById(R.id.bmiHeight);
+
+        energyKJ = view.findViewById(R.id.energyKJ);
+        energyCal = view.findViewById(R.id.energyCal);
+
+        Button calcKj = view.findViewById(R.id.calcKJ);
+        Button calcCal = view.findViewById(R.id.calcCal);
+        Button calcTDEE = view.findViewById(R.id.calcTDEEBtn);
+
+        activityLevel     = view.findViewById(R.id.activityLevelSp);
+
+        activityLevel.setAdapter(
+                new ArrayAdapter<>(
+                        activity,
+                        android.R.layout.simple_spinner_item,
+                        UnitConverter.PhysicalActivity.values()
+                )
+        );
+
+        calcKj.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                kjCalc();
+            }
+        });
+
+        calcCal.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                calCalc();
+            }
+        });
+
+        calcTDEE.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //TODO
+            }
+        });
+
+        //TODO - TDEE CALCULATOR
+
+        bmiHeight.addTextChangedListener(this);
+        energyKJ.addTextChangedListener(this);
+        energyCal.addTextChangedListener(this);
 
         return view;
     }
@@ -44,4 +109,39 @@ public class CalculatorFragment extends Fragment {
     public void setTDEE(int TDEE) {
         this.TDEE = TDEE;
     }
+
+    private void bmiCalc() {
+        if (bmiWeight.length() != 0 && bmiHeight.length() != 0) {
+            double weight = Double.parseDouble(bmiWeight.getText().toString());
+            double height = Double.parseDouble(bmiHeight.getText().toString());
+            bmiValue.setText(String.format("= %.1f", UnitConverter.getBMI(weight, height)));
+        }
+    }
+
+    public void kjCalc() {
+        if (energyCal.length() != 0) {
+            double calories = Double.parseDouble(energyCal.getText().toString());
+            energyKJ.setText(Double.toString(Math.round(UnitConverter.getKjFromCal(calories))), TextView.BufferType.EDITABLE);
+        }
+
+    }
+
+    public void calCalc() {
+        if (energyKJ.length() != 0) {
+            double kilojoules = Double.parseDouble(energyKJ.getText().toString());
+            energyCal.setText(Double.toString(Math.round(UnitConverter.getCalFromKj(kilojoules))), TextView.BufferType.EDITABLE);
+        }
+    }
+
+    // Text field updates
+    @Override
+    public void afterTextChanged(Editable editable){
+        bmiCalc();
+    }
+
+    //Unused callbacks
+    @Override
+    public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {}
+    @Override
+    public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {}
 }
